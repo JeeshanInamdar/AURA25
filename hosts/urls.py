f298d462dde50a8571fa99d31659065c08ac7222
PranavKdagadakai <pkdagadakai@gmail.com>
2024-10-13 18:10:58 +0530

    Apps merged

--- hosts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from hosts import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('validate/',views.qr_validation,name='qr_validation'),
-    path('check_qr_code/',views.check_qr_code,name='check_qr_code'),
-]
